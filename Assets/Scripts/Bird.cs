bb1d470612e6c2b247bc782661d5cfbe4bb48901
Isaiah Weaver <61598180+Caspian-X@users.noreply.github.com>
2021-06-08 14:26:31 -0700

    Bird Game

---
+++ Assets/Scripts/Bird.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bird : MonoBehaviour
+{
+    private Rigidbody2D rb;
+
+    public float increment;
+
+    public int Direction { get; private set; } //true = right, false = left
+    public int Score { get; private set; }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Direction = 1;
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+            if (rb.velocity.y < 0)
+                rb.velocity = Vector3.zero;
+            rb.AddForce(Vector2.up * rb.mass * 12, ForceMode2D.Impulse);
+            if (rb.velocity.y > 20)
+                rb.velocity = new Vector2(rb.velocity.x, 20);
+        }
+
+        gameObject.transform.position += gameObject.transform.right * Direction * increment;
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Wall")
+        {
+            Direction *= -1;
+            Vector3 scale = gameObject.transform.localScale;
+            scale.x *= -1;
+            gameObject.transform.localScale = scale;
+        }
+
+        if (collision.gameObject.tag == "Coin")
+        {
+            Score++;
+        }
+    }
+}
---
+++ Assets/Scripts/Score.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour
+{
+    private Bird bird;
+    private Text score;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        bird = GameObject.FindGameObjectWithTag("Player").GetComponent<Bird>();
+        score = gameObject.transform.GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        score.text = "Score: " + bird.Score;
+    }
+}
